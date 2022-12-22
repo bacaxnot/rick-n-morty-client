49f61ab83c248d5479fd0858666d3213cd21e747
JP <cala.js@icloud.com>
2022-12-22 13:58:46 -0500

    Minor refactor to API.test

--- src/tests/API.test.tsx
+++ src/tests/API.test.tsx
@@ -1,5 +1,5 @@
 import { API } from '@classes'
 
 describe('API Wrapper Class', () => {
-    it.skip('should fetch a list of all the characters', () => {})
+    it('should fetch a list of all the characters', () => {})
 })
